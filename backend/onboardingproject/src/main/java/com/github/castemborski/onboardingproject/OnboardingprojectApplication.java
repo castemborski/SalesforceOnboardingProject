55f69de17039e04bafb11b2a8ed57026e7412d2e
Collin Astemborski <cjastemb@ncsu.edu>
2026-07-23 19:38:53 -0400

    Adding Springboot framework

---
+++ backend/onboardingproject/src/main/java/com/github/castemborski/onboardingproject/OnboardingprojectApplication.java
@@ -0,0 +1,13 @@
+package com.github.castemborski.onboardingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingprojectApplication.class, args);
+	}
+
+}
